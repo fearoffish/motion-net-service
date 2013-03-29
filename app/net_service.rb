74066dd541f8d38d4506415af420edf215d6e576
Thomas Kadauke <tkadauke@imedo.de>
2013-03-29 12:19:56 +0100

    Extract NetService class

---
+++ app/net_service.rb
@@ -0,0 +1,67 @@
+class NetService
+  def initialize(name, options = {})
+    default_options = {
+      domain: "",
+      type: "_http._tcp.",
+    }
+    options.merge!(default_options)
+    
+    @net_service = NSNetService.alloc.initWithDomain options[:domain], type:options[:type], name:name, port:options[:port]
+    @net_service.setDelegate(self)
+    
+    if block_given?
+      yield self
+      publish
+    end
+  end
+  
+  def publish
+    @net_service.publish
+  end
+  
+  def resolve(timeout = 1)
+    @net_service.resolveWithTimeout(timeout)
+  end
+
+  [
+    :on_will_publish, :on_did_not_publish, :on_did_publish, :on_will_resolve, :on_did_not_resolve,
+    :on_did_resolve_address, :on_did_update_txt_record_data, :on_did_stop
+  ].each do |callback|
+    define_method callback do |&block|
+      instance_variable_set("@#{callback}", block)
+    end
+  end
+  
+protected
+  def netServiceWillPublish(sender)
+    @on_publish.call if @on_publish
+  end
+  
+  def netService(sender, didNotPublish:errors)
+    @on_did_not_publish.call(errors) if @on_did_not_publish
+  end
+  
+  def netServiceDidPublish(sender)
+    @on_did_publish.call if @on_did_publish
+  end
+  
+  def netServiceWillResolve(sender)
+    @on_will_resolve.call if @on_will_resolve
+  end
+  
+  def netService(sender, didNotResolve:errors)
+    @on_did_not_resolve.call(errors) if @on_did_not_resolve
+  end
+  
+  def netServiceDidResolveAddress(sender)
+    @on_did_resolve_address.call if @on_did_resolve_address
+  end
+  
+  # def netService(sender, didUpdateTXTRecordData:data)
+  #   @on_did_update_txt_record_data.call(data) if @on_did_update_txt_record_data
+  # end
+  
+  def netServiceDidStop(sender)
+    @on_did_stop.call if @on_did_stop
+  end
+end
